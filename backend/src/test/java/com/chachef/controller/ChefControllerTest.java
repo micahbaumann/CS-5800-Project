2e2c3d36faecfa9beb83c01a7d4db612b6312631
Nicholas Tsimerekis <ntsimerekis@protonmail.com>
2025-10-22 21:54:30 -0700

    more unit test stuff

--- backend/src/test/java/com/chachef/controller/ChefControllerTest.java
+++ backend/src/test/java/com/chachef/controller/ChefControllerTest.java
@@ -1,9 +1,11 @@
 package com.chachef.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ChefControllerTest {
 
     @Test
--- backend/src/test/java/com/chachef/service/ChefServiceTest.java
+++ backend/src/test/java/com/chachef/service/ChefServiceTest.java
@@ -1,9 +1,11 @@
 package com.chachef.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ChefServiceTest {
 
     @Test
